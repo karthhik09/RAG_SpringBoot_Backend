0bb03cc0d92a7d3e2a27dfba182fd7b75f6ac570
karthhik09 <karthiknandeti@gmail.com>
2026-08-14 00:37:51 +0530

    stopped tracking properties file  and added to gitignore

--- src/main/java/com/project/rag/security/UserDetailsService.java
+++ src/main/java/com/project/rag/security/UserDetailsService.java
@@ -1,4 +1,14 @@
 package com.project.rag.security;
 
+import com.project.rag.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    
 }
