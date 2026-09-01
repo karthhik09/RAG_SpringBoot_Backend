3de1cf8a31bc5df27301414d41787be8de4566c0
karthhik09 <karthiknandeti@gmail.com>
2026-09-01 20:39:42 +0530

    updated method with non null annotation and for unchecked assignment replaced Collections with List

--- src/main/java/com/project/rag/security/UserDetailsService.java
+++ src/main/java/com/project/rag/security/UserDetailsService.java
@@ -2,11 +2,12 @@ package com.project.rag.security;
 
 import com.project.rag.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +17,10 @@ public class UserDetailsService implements org.springframework.security.core.use
 
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public @NonNull UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
                 .map(user -> new org.springframework.security.core.userdetails.User
-                        (user.getEmail(), user.getPassword(), Collections.EMPTY_LIST)
+                        (user.getEmail(), user.getPassword(), List.of())
                 )
                 .orElseThrow(() -> new UsernameNotFoundException("user not found" + email));
     }
