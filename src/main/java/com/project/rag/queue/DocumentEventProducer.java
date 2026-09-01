132e36ce3b9a764e369ea1878537cbd28716db05
karthhik09 <karthiknandeti@gmail.com>
2026-09-01 20:50:44 +0530

    added doc event producer to push to redis queue

--- src/main/java/com/project/rag/queue/DocumentEventProducer.java
+++ src/main/java/com/project/rag/queue/DocumentEventProducer.java
@@ -1,4 +1,24 @@
 package com.project.rag.queue;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import tools.jackson.databind.ObjectMapper;
+
+@Service
+@RequiredArgsConstructor
 public class DocumentEventProducer {
+
+    private final StringRedisTemplate stringRedisTemplate;
+    private final ObjectMapper objectMapper;
+    private static final String QUEUE_NAME = "document_ingestion_queue";
+
+    public void publishEvent(DocumentEvent event) {
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(event);
+            stringRedisTemplate.opsForList().leftPush(QUEUE_NAME, jsonMessage);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to serialise event for Redis", e);
+        }
+    }
 }
