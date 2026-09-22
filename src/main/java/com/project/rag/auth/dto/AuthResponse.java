398087c1db4cc91eb6e1512b2bcab44c16553b04
karthhik09 <karthiknandeti@gmail.com>
2026-09-22 23:52:13 +0530

    changed type from integer to long for id

--- src/main/java/com/project/rag/auth/dto/AuthResponse.java
+++ src/main/java/com/project/rag/auth/dto/AuthResponse.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class AuthResponse {
 
     private String token;
-    private Integer id;
+    private Long id;
     private String name;
     private String email;
 }
