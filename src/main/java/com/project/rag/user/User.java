27427f811bb03150a6954c9f85a11a0cfb8a4eaa
karthhik09 <karthiknandeti@gmail.com>
2026-09-23 00:01:47 +0530

    replaced data with getter and setter annotations

--- src/main/java/com/project/rag/user/User.java
+++ src/main/java/com/project/rag/user/User.java
@@ -1,13 +1,11 @@
 package com.project.rag.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
