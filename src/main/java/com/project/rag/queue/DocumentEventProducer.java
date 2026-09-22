149ae72810e244a67fd336dab036392695c504e5
karthhik09 <karthiknandeti@gmail.com>
2026-09-23 00:16:33 +0530

    fixed invalid objectmapper import and initialised directly

--- src/main/java/com/project/rag/queue/DocumentEventProducer.java
+++ src/main/java/com/project/rag/queue/DocumentEventProducer.java
@@ -1,16 +1,18 @@
 package com.project.rag.queue;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
-import tools.jackson.databind.ObjectMapper;
 
 @Service
 @RequiredArgsConstructor
 public class DocumentEventProducer {
 
     private final StringRedisTemplate stringRedisTemplate;
-    private final ObjectMapper objectMapper;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     private static final String QUEUE_NAME = "document_ingestion_queue";
 
     public void publishEvent(DocumentEvent event) {
