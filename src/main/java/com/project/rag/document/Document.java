187f2deb29f2276508988edb4f53e8c63525b472
karthhik09 <karthiknandeti@gmail.com>
2026-09-22 23:59:59 +0530

    updated type to Long for userId and replaced data with setter and getter annotations

--- src/main/java/com/project/rag/document/Document.java
+++ src/main/java/com/project/rag/document/Document.java
@@ -1,17 +1,15 @@
 package com.project.rag.document;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -22,7 +20,7 @@ public class Document {
     private Long id;
 
     @Column(nullable = false)
-    private Integer userId;
+    private Long userId;
 
     @Column(nullable = false)
     private String fileName;
